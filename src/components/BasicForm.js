0fb737ed2b28c8f174f38ce0c1ff5693e8398481
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-02-22 16:43:33 +0300

    Updated code

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -57,18 +57,18 @@ const BasicForm = (props) => {
         <div className={firstNameClasses}>
           <label htmlFor='name'>First Name</label>
           <input type='text' id='name' value={firstNameValue} onChange={firstNameChangeHandler} onBlur={firstNameBlurHandler}/>
-          {firstNameHasError && <p>Please enter a first name</p>}
+          {firstNameHasError && <p className="error-text">Please enter a first name</p>}
         </div>
         <div className={lastNameClasses}>
           <label htmlFor='name'>Last Name</label>
           <input type='text' id='name' value={lastNameValue} onChange={lastNameChangeHandler} onBlur={lastNameBlurHandler} />
-          {lastNameHasError && <p>Please enter a last name</p>}
+          {lastNameHasError && <p className="error-text">Please enter a last name</p>}
         </div>
       </div>
       <div className={emailClasses}>
         <label htmlFor='name'>E-Mail Address</label>
         <input type='text' id='name' value={emailValue} onChange={emailChangeHandler} onBlur={emailBlurHandler} />
-        {emailHasError && <p>Please enter a valid email Address</p>}
+        {emailHasError && <p className="error-text">Please enter a valid email Address</p>}
       </div>
       <div className='form-actions'>
         <button disabled={!formIsValid}>Submit</button>
